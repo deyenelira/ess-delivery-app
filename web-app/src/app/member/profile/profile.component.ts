4b936d52a146204a30dfd71341ce1f00d8a8f323
Ubuntu <ubuntu@ubuntu2110.linuxvmimages.local>
2022-04-04 19:07:48 -0400

    Perfil com dados e imagem bugada

--- web-app/src/app/member/profile/profile.component.ts
+++ web-app/src/app/member/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ClientService } from "src/app/client/client.service";
 
 @Component({
   selector: "profile",
@@ -6,8 +7,23 @@ import { Component } from "@angular/core";
   styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent {
+  name:string;
+  cpf:string;
+  telefone:string;
+  enderecos:string[];
+  metodos_pag:string;
+  
 
-  constructor() {
-    console.log('criou');
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.clientService.getClient().then((result) => {
+      this.name = result.name;
+      this.cpf = result.cpf
+      this.telefone = result.phone
+      this.enderecos = result.addresses
+      this.metodos_pag = result.pay_method
+    });
   }
+  
 }
