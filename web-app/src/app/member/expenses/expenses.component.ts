ecaae5b8b11028761eb985e025f295d4664bc562
Matheus Godinho <mrbg@cin.ufpe.br>
2022-04-08 20:51:19 -0300

    Fazendo a parte de front dos graficos

--- web-app/src/app/member/expenses/expenses.component.ts
+++ web-app/src/app/member/expenses/expenses.component.ts
@@ -7,50 +7,296 @@ import { Component } from "@angular/core";
 })
 
 export class ExpensesComponent{
-  options: any;
-  constructor() {}
-
-  ngOnInit(): void {
-    const xAxisData = [];
-    const data1 = [];
-    const data2 = [];
-
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+  data = [
+    {
+      name: 'Apples',
+      value: 70
+    },
+    {
+      name: 'Strawberries',
+      value: 68
+    },
+    {
+      name: 'Bananas',
+      value: 48
+    },
+    {
+      name: 'Oranges',
+      value: 40
+    },
+    {
+      name: 'Pears',
+      value: 32
+    },
+    {
+      name: 'Pineapples',
+      value: 27,
+    },
+    {
+      name: 'Grapes',
+      value: 18
     }
-
+  ];
+  options: any;
+  initOpts: any;
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.initOpts = {
+      renderer: "canvas",
+      width: 1282*devicePixelRatio,
+      height: 1280*devicePixelRatio
+    };
+  
     this.options = {
-      legend: {
-        data: ['bar', 'bar2'],
-        align: 'left',
-      },
-      tooltip: {},
-      xAxis: {
-        data: xAxisData,
-        silent: false,
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {},
-      series: [
+      //Settando os titulos
+      
+      title: [
+        //Comida mais cara 
         {
-          name: 'bar',
-          type: 'pie',
-          data: data1,
-          animationDelay: (idx: number) => idx * 10,
+          text: 'Comida mais cara',
+          textStyle:{
+            fontFamily: 'ABeeZee Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 32,
+            color: "#FFFFFF",
+          },
+          left: '75%',
+          top: '5%',
+          textAlign: 'center',
         },
         {
-          name: 'bar2',
-          type: 'pie',
-          data: data2,
-          animationDelay: (idx: number) => idx * 10 + 100,
+          subtext: 'Comidaaaaaaaaaaaa mais caraaaaaaaaaaaaaaaaaaaa',
+          subtextStyle:{
+            fontFamily: 'Cherry Bomb One Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 24,
+            color: '#FF5200',
+            width: 450,
+            overflow: 'break',
+          },
+          left: '75%',
+          top: '7.5%',
+          textAlign: 'center',
+        },
+
+        //Restaurante mais caro
+        {
+          text: 'Restaurante mais caro',
+          textStyle:{
+            fontFamily: 'ABeeZee Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 32,
+            color: "#FFFFFF",
+          },
+          left: '25%',
+          top: '5%',
+          textAlign: 'center',
+        },
+        {
+          subtext: 'Restaurante mais caro',
+          subtextStyle:{
+            fontFamily: 'Cherry Bomb One Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 24,
+            color: '#FF5200',
+            width: 450,
+            overflow: 'break',
+          },
+          left: '25%',
+          top: '7.5%',
+          textAlign: 'center'
+        },
+
+        //Comida mais comprada
+        {
+          text: 'Comida mais comprada',
+          textStyle:{
+            fontFamily: 'ABeeZee Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 32,
+            color: "#FFFFFF",
+          },
+          left: '75%',
+          top: '55%',
+          textAlign: 'center',
+        },
+        {
+          subtext: 'Comida mais comprada',
+          subtextStyle:{
+            fontFamily: 'Cherry Bomb One Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 24,
+            color: '#FF5200',
+            width: 450,
+            overflow: 'break',
+          },
+          left: '75%',
+          top: '57.5%',
+          textAlign: 'center'
+        },
+
+        //Restaurante mais comprado
+        {
+          text: 'Restaurante mais comprado',
+          textStyle:{
+            fontFamily: 'ABeeZee Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 32,
+            color: "#FFFFFF",
+          },
+          left: '25%',
+          top: '55%',
+          textAlign: 'center',
+        },
+        {
+          subtext: 'Restaurante mais comprado',
+          subtextStyle:{
+            fontFamily: 'Cherry Bomb One Regular',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: 24,
+            color: '#FF5200',
+            width: 450,
+            overflow: 'break',
+          },
+          left: '25%',
+          top: '57.5%',
+          textAlign: 'center'
         },
       ],
-      animationEasing: 'elasticOut',
-      animationDelayUpdate: (idx: number) => idx * 5,
+
+      tooltip: {
+        trigger: "item",
+        width: "100%"
+      },
+      legend: {
+        show: false,
+      },
+      series: [
+        //Chart da comida mais cara
+        {
+          name: "Comida:",
+          type: "pie",
+          radius: "25%",
+          center: ['75%', '25%'],
+          data: this.data,
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: "#fff",
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: "center"
+          },
+          emphasis: {
+            label: {
+              show: false,
+              fontSize: "40",
+              fontWeight: "bold"
+            }
+          },
+          labelLine: {
+            show: false
+          },
+        },
+        
+        //Chart da comida mais comprada
+        {
+          name: "Comida:",
+          type: "pie",
+          radius: "25%",
+          center: ['75%', '75%'],
+          data: this.data,
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: "#fff",
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: "center"
+          },
+          emphasis: {
+            label: {
+              show: false,
+              fontSize: "40",
+              fontWeight: "bold"
+            }
+          },
+          labelLine: {
+            show: false
+          },
+        },
+
+        //Chart do restaurante mais caro
+        {
+          name: "Restaurante:",
+          type: "pie",
+          radius: "25%",
+          center: ['25%', '25%'],
+          data: this.data,
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: "#fff",
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: "center"
+          },
+          emphasis: {
+            label: {
+              show: false,
+              fontSize: "40",
+              fontWeight: "bold"
+            }
+          },
+          labelLine: {
+            show: false
+          },
+        },
+        
+        //Chart do restaurante mais comprado
+        {
+          name: "Restaurante:",
+          type: "pie",
+          radius: "25%",
+          center: ['25%', '75%'],
+          data: this.data,
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: "#fff",
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: "center"
+          },
+          emphasis: {
+            label: {
+              show: false,
+              fontSize: "40",
+              fontWeight: "bold"
+            }
+          },
+          labelLine: {
+            show: false
+          },
+        },
+      ]
     };
   }
 }
