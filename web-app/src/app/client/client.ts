5ff1faf5d9837c505d374d3eeb7bf14b183d6102
Ana <analeticia1101@gmail.com>
2022-04-10 19:02:13 -0300

    ajudes esqueci a senha

--- web-app/src/app/client/client.ts
+++ web-app/src/app/client/client.ts
@@ -7,19 +7,23 @@ export class Client {
   password:  string="";
   pay_method: string="";
   addresses: string[]=[];
+  code:  string="";
+  validPhone: boolean= false;
 
    constructor(name?: string, 
     cpf?: string,
     email?: string,
     phone?: string,
     password?: string) {
-    this.name = name ?? "";
-    this.cpf = cpf ?? "";
-    this.phone = phone ?? "";
-    this.email = email ?? "";
-    this.password = password ?? "";
-    this.pay_method = "";
-    this.addresses = [];
+      this.name = name ?? "";
+      this.cpf = cpf ?? "";
+      this.phone = phone ?? "";
+      this.email = email ?? "";
+      this.password = password ?? "";
+      this.pay_method = "";
+      this.addresses = [];
+      this.code = "";
+      this.validPhone = false;
     }
   
 }
